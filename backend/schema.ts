2839072cf4d296ffab8bdc80766988c0e32d7acd
Dmytro Danylov <d-dmytro@users.noreply.github.com>
2022-10-08 13:08:58 +0300

    Fixed 'schema' undefined error This error appears after updating dependencies to latest

--- backend/schema.ts
+++ backend/schema.ts
@@ -1,6 +1,7 @@
-import { makeExecutableSchema } from 'graphql-tools';
 import { typeDefs } from './type-defs';
 import { resolvers } from './resolvers';
+
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 export const schema = makeExecutableSchema({
   typeDefs,
