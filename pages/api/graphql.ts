3989cb6f206ca8c36f7ed5f130e73e06c3438e93
Dmytro Danylov <d-dmytro@users.noreply.github.com>
2022-10-08 13:20:05 +0300

    Fix the Apollo Server setup after upgrading apollo-server-micro

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,8 +1,8 @@
+import { NextApiHandler } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { db } from '../../backend/db';
 import { schema } from '../../backend/schema';
-
-const apolloServer = new ApolloServer({ schema, context: { db } });
 
 export const config = {
   api: {
@@ -10,4 +10,28 @@ export const config = {
   },
 };
 
-export default apolloServer.createHandler({ path: '/api/graphql' });
+const apolloServer = new ApolloServer({
+  schema,
+  context: { db },
+  // Display old playground web app when opening http://localhost:3000/api/graphql in the browser
+  plugins: [
+    ...(process.env.NODE_ENV === 'development'
+      ? [ApolloServerPluginLandingPageGraphQLPlayground]
+      : []),
+  ],
+});
+
+// Now we need to start Apollo Server before creating the handler function.
+const serverStartPromise = apolloServer.start();
+let graphqlHandler: NextApiHandler | undefined;
+
+const handler: NextApiHandler = async (req, res) => {
+  if (!graphqlHandler) {
+    await serverStartPromise;
+    graphqlHandler = apolloServer.createHandler({ path: '/api/graphql' });
+  }
+
+  return graphqlHandler(req, res);
+};
+
+export default handler;
